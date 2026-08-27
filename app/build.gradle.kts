8a6c2ce459ab03a84c6ac89be420645a9fda51be
choivoo <choivoovooge@gmail.com>
2026-08-27 16:51:24 +0900

    release(v2.3.2): bump speech recognizer hotfix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.choivoo.jarvis"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "2.3.1"
+        versionCode = 27
+        versionName = "2.3.2"
 
         buildConfigField("String", "JARVIS_API_BASE_URL", quotedBuildConfig(jarvisApiBaseUrl))
         buildConfigField("String", "JARVIS_APP_TOKEN", quotedBuildConfig(jarvisAppToken))
